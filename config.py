994d5146de92a6dbda2bf6b4be6616093c5aafbd
Jwvskjsywu7 <Aryan82586@outlook.com>
2024-11-15 22:07:04 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VIPMODXOWNER
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7819992909:AAHn51FAfPId42gmKUT5wPmCoyC4_g9OeN0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@GODxAloneBOY")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1662672529")
